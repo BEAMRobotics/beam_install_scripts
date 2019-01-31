edccb9363ea9a790c1ee8b7819832f9d5655331e
Evan McLaughlin <37314371+emmclaugh@users.noreply.github.com>
2019-01-31 17:59:57 -0500

    fix, ros install index was to incorrect directory

--- beam_install.bash
+++ beam_install.bash
@@ -57,11 +57,11 @@ install_routine()
 
     # submodule_init
 
-    bash ros_install/ros_install.bash
+    bash ros_install.bash
     bash create_catkin_workspace.bash
 
     link_routine
-    bash ros_install/rosdeps_install.bash
+    bash rosdeps_install.bash
 
     bash beam_dependencies_install.bash
 
