4418a4f0121c49e0a57c31cad074faea8f3f734b
nickcharron <nicholas.c.charron@gmail.com>
2019-02-11 20:01:13 -0500

    fixing a bunch of errors
    tested and seems to install everything correctly. Still need to test sensors on robot.

--- robot_hardware_install.bash
+++ robot_hardware_install.bash
@@ -10,6 +10,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 main()
 {
+    clone_ros_drivers
     install_ximea_deps
     update_udev
     install_gps
@@ -18,11 +19,37 @@ main()
     install_libpcap
     install_husky_packages
     enable_passwordless_sudo
+    catkin_build
+}
+
+catkin_build()
+{
+    cd /home/"$USER"/catkin_ws
+    catkin build
+}
+
+clone_ros_drivers()
+{
+    if [ ! -d /home/"$USER"/projects ]; then
+    	mkdir /home/"$USER"/projects
+    fi
+    if [ -d /home/"$USER"/projects/ros_drivers ]; then
+        echo "ros_drivers already installed in /home/"$USER"/projects/"
+        echo "make sure there is a symlink in catkin_ws"
+    fi
+    if [ ! -d /home/"$USER"/projects/ros_drivers ]; then
+        cd /home/"$USER"/projects
+        echo "installing ros_drivers to /home/"$USER"/projects"
+        git clone --recursive https://github.com/BEAMRobotics/ros_drivers.git
+        echo "creating link in /home/"$USER"/catkin_ws/src/ to /home/"$USER"/projects"
+        ln -s /home/"$USER"/projects/ros_drivers /home/"$USER"/catkin_ws/src/
+    fi
 }
 
 
 install_ximea_deps()
 {
+    echo "installing ximea dependencies..."
     cd ~/
     mkdir tmp
     cd tmp
@@ -46,69 +73,66 @@ install_ximea_deps()
 
 update_udev()
 {
-    # copy udev rules from inspector_gadget   
-    sudo cp ~/catkin_ws/src/beam_robotics/inspector_gadget/udev/* /etc/udev/rules.d/
+    # copy udev rules from inspector_gadget 
+    echo "copying udev rules..."  
+    sudo cp ~/catkin_ws/src/ros_drivers/udev/* /etc/udev/rules.d/
     sudo udevadm control --reload-rules && sudo service udev restart && sudo udevadm trigger
 }
 
 install_gps()
 {
+    echo "installing piksi gps dependencies.."
     cd ~/
     mkdir -p software
     cd software # cd to a directory where you will download and build libsbp
     git clone https://github.com/swift-nav/libsbp 
     cd libsbp/c
     mkdir build
-    cmake ../
+    cd build
+    cmake ..
     make
     sudo make install # install headers and libraries into /usr/local
     
-    echo 'Please enter path to inspector_gadget'
-    echo 'Example: ~/projects/inspector_gadget'
-    read IG_PATH
-    bash $IG_PATH/inspector_gadget/ros_drivers/ethz_piksi_ros/piksi_multi_rtk_ros/install/install/piksi_multi.sh
-
-    cd ~/catkin_ws/src
-    git clone https://github.com/nickcharron/swiftnav_ros.git
-    cd ..
-    catkin_make
+    bash /home/"$USER"/catkin_ws/src/ros_drivers/ethz_piksi_ros/piksi_multi_rtk_ros/install/install_piksi_multi.sh
 }
 
 install_um7()
 {
+    echo "installing um7 driver..."
     sudo apt-get install ros-kinetic-um7 #install ros driver
     sudo apt-get install ros-kinetic-geographic-msgs
 }
 
 install_flir_blackfly()
 {
-    echo 'Please enter path to flir_camera_driver'
-    echo 'Example: ~/projects/beam_robotics/rps_drivers/flir_camera_driver'
-    read FLIR_PATH
-    sudo bash $FLIR_PATH/flir_camera_driver/spin-conf.sh
+    echo "configuring spinnaker driver..."
+    sudo bash /home/"$USER"/catkin_ws/src/ros_drivers/flir_camera_driver/spin-conf
 }
 
 install_libpcap()
 {
     # for velodyne driver
+    echo "installing velodyne driver dependencies..."
     sudo apt-get install libpcap-dev
 }
 
 install_husky_packages()
 {
-    sudo apt-get install ros-kinetic-controller-manager*
-    ros-kinetic-teleop-*
-    ros-kinetic-twist-mux*
-    ros-kinetic-lms1xx
-    ros-kinetic-ur-description
-    ros-kinetic-joint-state-publisher
-    ros-kinetic-joint-state-controller
+    echo "installing husky dependencies..."
+    sudo apt-get install ros-kinetic-controller-manager* \
+    ros-kinetic-teleop-* \
+    ros-kinetic-twist-mux* \
+    ros-kinetic-lms1xx \
+    ros-kinetic-ur-description \
+    ros-kinetic-joint-state-publisher \
+    ros-kinetic-joint-state-controller \
     ros-kinetic-diff-drive-controller   
 }
 
 enable_passwordless_sudo()
 {
-    echo 'robot ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers 
+    #sudo echo 'robot ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers 
+    echo "robot ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
 }
 
 main
