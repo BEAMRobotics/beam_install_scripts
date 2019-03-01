1aef47ec4fa9282af9496e02b9440c572d0d45fc
emmclaugh <emmclaugh@uwaterloo.ca>
2019-03-01 10:05:30 -0500

    factor our common link routine code

--- link_routines.bash
+++ link_routines.bash
@@ -1,18 +1,10 @@
 #!/bin/bash
 set -e
 
-## Install scripts directory
-#INSTALL_SCRIPTS=$"$HOME/software/beam_install_scripts"
 
-## get UBUNTU_CODENAME, ROS_DISTRO, CATKIN_DIR
-#source $INSTALL_SCRIPTS/identify_environment.bash
-
-## set up symlink
-#: ${SYMLINKS_REPO_DIR:=$REPO_DIR}
-
-## link repo being installed to catkin_ws
 link_routine()
 {
+    # link repo being installed to catkin_ws
     ln -sfn "$SYMLINKS_REPO_DIR" "$CATKIN_DIR/src"
     echo "Symlink to $SYMLINKS_REPO_DIR created successfully"
 }
