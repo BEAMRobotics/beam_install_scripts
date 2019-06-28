82931bf65bee1b4295d99683aeef5217490c31b3
Evan McLaughlin <37314371+emmclaugh@users.noreply.github.com>
2019-06-28 16:15:02 -0400

    Update robot_hardware_install.bash

--- robot_hardware_install.bash
+++ robot_hardware_install.bash
@@ -10,7 +10,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 main()
 {
-    clone_ros_drivers
+    # clone_ros_drivers
     # install_ximea_deps
     update_udev
     install_gps
