6c33889dec4e4a6547d57172f3ca186c5c6e904a
Evan McLaughlin <37314371+emmclaugh@users.noreply.github.com>
2019-07-29 15:36:32 -0400

    turn off ml

--- beam_dependencies_install.bash
+++ beam_dependencies_install.bash
@@ -130,7 +130,7 @@ install_pcl()
               -DBUILD_CUDA=OFF -DBUILD_GPU=OFF \
               -DBUILD_tracking=OFF -DBUILD_people=OFF -DBUILD_features=OFF \
 	      -DBUILD_octree=OFF -DBUILD_recognition=OFF -DBUILD_test=OFF\
-	      -DBUILD_keypoints=OFF \
+	      -DBUILD_keypoints=OFF -DBUILD_ml=OFF \
               -DBUILD_stereo=OFF -DBUILD_simulation=OFF -DBUILD_apps=OFF \
               -DBUILD_examples=OFF -DBUILD_tools=OFF -DBUILD_visualization=ON"
     fi
