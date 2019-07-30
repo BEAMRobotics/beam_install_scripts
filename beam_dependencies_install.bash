07320525b0500096f477d06af660e90d7a7cda93
Evan McLaughlin <37314371+emmclaugh@users.noreply.github.com>
2019-07-30 13:26:18 -0400

    fix pcl make args

--- beam_dependencies_install.bash
+++ beam_dependencies_install.bash
@@ -128,9 +128,9 @@ install_pcl()
               -DWITH_LIBUSB=OFF -DWITH_OPENGL=OFF -DWITH_OPENNI=OFF \
               -DWITH_OPENNI2=OFF -DWITH_QT=OFF -DWITH_RSSDK=OFF \
               -DBUILD_CUDA=OFF -DBUILD_GPU=OFF \
-              -DBUILD_tracking=OFF -DBUILD_people=OFF -DBUILD_features=OFF -DBUILD_doc=OFF \
-	      -DBUILD_octree=OFF -DBUILD_recognition=OFF -DBUILD_test=OFF -DBUILD_outofcore=OFF \
-	      -DBUILD_keypoints=OFF -DBUILD_ml=OFF -DBUILD_2D=OFF -DBUILD_geometry=OFF \
+              -DBUILD_tracking=OFF -DBUILD_people=OFF -DBUILD_doc=OFF \
+	      -DBUILD_recognition=OFF -DBUILD_test=OFF -DBUILD_outofcore=OFF \
+	      -DBUILD_keypoints=OFF \
               -DBUILD_stereo=OFF -DBUILD_simulation=OFF -DBUILD_apps=OFF \
               -DBUILD_examples=OFF -DBUILD_tools=OFF -DBUILD_visualization=ON"
     fi
