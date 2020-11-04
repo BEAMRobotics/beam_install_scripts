30e87bf958fe0197475e09fefe0226439f1b5898
Jake McLaughlin <jake.mclaughlin98@gmail.com>
2020-11-04 10:36:22 -0500

    Add dbow3 install

--- beam_dependencies_install.bash
+++ beam_dependencies_install.bash
@@ -418,3 +418,27 @@ install_ladybug_sdk()
         echo "Ladybug SDK successfully installed in /usr/local/"
     fi
 }
+
+
+install_dbow3()
+{
+  DBOW_DIR="dbow"
+  BUILD_DIR="build"
+  mkdir -p $DEPS_DIR
+  cd $DEPS_DIR
+
+  if [ ! -d "$DBOW_DIR" ]; then
+    git clone git@github.com:rmsalinas/DBow3.git
+  fi
+
+  cd $FBOW_DIR
+  if [ ! -d "$BUILD_DIR" ]; then
+    mkdir -p $BUILD_DIR
+    cd $BUILD_DIR
+    cmake ..
+    make -j$(nproc)
+  fi
+
+  cd $DEPS_DIR/$DBOW_DIR/$BUILD_DIR
+  sudo make -j$(nproc) install
+}
--- install.bash
+++ install.bash
@@ -53,6 +53,7 @@ install_routine()
     #install_libwave
     install_json
     install_ladybug_sdk
+    install_dbow3
 
     # check that ros installed correctly
     ROS_CHECK="$(rosversion -d)"
