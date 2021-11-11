ac026178e912a67140a6b0e9907fb6803c15c2bd
nickcharron <nicholas.c.charron@gmail.com>
2021-11-10 21:19:59 -0800

    update rosdep install

--- rosdeps_install.bash
+++ rosdeps_install.bash
@@ -12,10 +12,10 @@ rosdep update > /dev/null
 # Install system dependencies listed in ROS packages' package.xml
 # Note: dependencies needed on embedded systems must still be included
 # separately in the repo or cross-compiled stage.
-if [ -d "$ROSPACKAGES_DIR" ]; then
-    # we have a source stack distro
-    rosdep install -qry --from-paths $CATKIN_DIR/src/ --ignore-src
-else
-    # we have a binary stack distro
-    rosdep install -qry --from-paths $REPO_DIR --ignore-src
-fi
+#if [ -d "$ROSPACKAGES_DIR" ]; then
+#    # we have a source stack distro
+#    rosdep install -qry --from-paths $CATKIN_DIR/src/ --ignore-src
+#else
+#    # we have a binary stack distro
+#    rosdep install -qry --from-paths $REPO_DIR --ignore-src
+#fi
